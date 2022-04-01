437a82ec554c2a68cd55afdd8b1c11ada7792424
ediprogramando <101778961+ediprogramando@users.noreply.github.com>
2022-03-31 21:29:19 -0600

    Update sum_two_numbers.js

--- basics/sum_two_numbers.js
+++ basics/sum_two_numbers.js
@@ -1,12 +1,8 @@
 // Write a function that takes two numbers (a and b) as argument
 // Sum a and b
 // Return the result
-function
-myFunction
-(
-a, b
-)
-{
 
-return a+b;
+function myFunction(a, b) {
+
+   return a+b;
 }
