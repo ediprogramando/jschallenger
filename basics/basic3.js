e9945fbb49c0752ce16e0fb720949dae8a0b46c6
ediprogramando <101778961+ediprogramando@users.noreply.github.com>
2022-03-31 21:48:56 -0600

    Create basic3.js

---
+++ basics/basic3.js
@@ -0,0 +1,8 @@
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+
+function myFunction(str) {
+
+   return str[str.length - 3] + str[str.length - 2] + str[str.length - 1];
+}
