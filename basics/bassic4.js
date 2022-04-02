5eed8a1ed92ea5ef9c7e127cdb859ea7445b7ef4
ediprogramando <101778961+ediprogramando@users.noreply.github.com>
2022-04-01 19:38:51 -0600

    Create bassic4.js

---
+++ basics/bassic4.js
@@ -0,0 +1,13 @@
+// https://www.jschallenger.com/javascript-basics/get-first-characters-string-javascript
+function myFunction(a) {
+   
+   return a[0] + a[1] + a[2];
+   
+   
+}
+
+/*
+function myFunction(a) {
+   return a.slice(0, 3);
+}
+*/
