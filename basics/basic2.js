026f9ee2cf83d9724ec433e786ee631de659a912
ediprogramando <101778961+ediprogramando@users.noreply.github.com>
2022-04-01 20:01:37 -0600

    Update basic2.js

--- basics/basic2.js
+++ basics/basic2.js
@@ -1,6 +1,6 @@
 // Write a function that takes two values, say a and b, as arguments
 // Return true if the two values are equal and of the same type
-// https://www.jschallenger.com/javascript-basics/sum-two-numbers-javascript
+// https://www.jschallenger.com/javascript-basics/comparison-strict-equality
 function
 myFunction
 (
