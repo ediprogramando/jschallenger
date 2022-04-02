ab568cf97f66a8f18f041bbb20316ae1a3963fbf
ediprogramando <101778961+ediprogramando@users.noreply.github.com>
2022-04-01 20:08:07 -0600

    Update type-value-javascript.js

--- basics/type-value-javascript.js
+++ basics/type-value-javascript.js
@@ -1,3 +1,6 @@
+// Write a function that takes a value as argument
+// Return the type of the value
+
 // https://www.jschallenger.com/javascript-basics/type-value-javascript
 function myFunction(a) {
    
