e24da1db7ec5b1af86beb5623e7d22becc1547b4
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-04-02 21:02:13 -0600

    aggregar solution para  https://www.jschallenger.com/javascript-basics/get-nth-character-string-javascript.

---
+++ basics/get-nth-character-string-javascript.js
@@ -0,0 +1,9 @@
+// https://www.jschallenger.com/javascript-basics/get-nth-character-string-javascript
+
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+
+function myFunction (a, n)
+{
+    return a [n - 1];
+}
