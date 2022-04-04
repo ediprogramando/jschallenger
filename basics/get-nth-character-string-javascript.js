17181bf2920641b0efd8bcc73ed4d770d7ad931d
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-04-03 21:02:13 -0600

    practica

--- basics/get-nth-character-string-javascript.js
+++ basics/get-nth-character-string-javascript.js
@@ -7,3 +7,32 @@ function myFunction (a, n)
 {
     return a [n - 1];
 }
+
+/*
+myFunction('abcd',1)
+Expected
+'a'
+
+myFunction('zyxbwpl',5)
+Expected
+'w'
+
+myFunction('gfedcba',3)
+Expected
+'e'
+
+*/
+
+const valores = [
+    ['abcd', 1, 'a'],
+    ['zyxbwpl', 5, 'w'],
+    ['fgedcba', 3, 'e']
+ ];
+
+ for (var i = 0; i < valores.length; i++)
+ { result = myFunction(valores [i] [0], valores [i] [1]); 
+    console.log ("resultado: " + result + " , " + valores [i] [2]);
+
+
+}
+ 
