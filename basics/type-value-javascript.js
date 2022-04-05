c2364e8e324408123896279d2624bb8442add4b1
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-04-04 22:03:43 -0500

    programa

--- basics/type-value-javascript.js
+++ basics/type-value-javascript.js
@@ -4,5 +4,56 @@
 // https://www.jschallenger.com/javascript-basics/type-value-javascript
 function myFunction(a) {
    
-   return typeof a;
+   return typeof(a);
 }
+
+
+/*
+myFunction(1)
+Expected
+'number'
+
+myFunction(false)
+Expected
+'boolean'
+
+myFunction({})
+Expected
+'object'
+
+myFunction(null)
+Expected
+'object'
+
+myFunction('string')
+Expected
+'string'
+
+myFunction(['array'])
+Expected
+'object'
+*/
+
+const valores = [
+   [1, 'number'],
+   [false, 'boolean'],
+   [{}, 'object'],
+   [null, 'object'],
+   ['string', 'string'],
+   [['array'], 'object']
+];
+
+
+for (var i = 0; i < valores.length; i++) {
+   result = myFunction(valores[i][0]);
+   console.log("resultado: " + result + ", " + valores[i][1]);
+}
+
+
+/*
+console.log (typeof('1'));
+console.log (typeof('false'));
+console.log (typeof('{}'));
+console.log (typeof('string'));
+console.log (typeof('array'));
+*/
