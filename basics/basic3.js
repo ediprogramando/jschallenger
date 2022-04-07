b3497749d705738b1ee3a00aeeac044ba9bbdb9b
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-04-06 20:21:49 -0500

    basic2mejorado

--- basics/basic3.js
+++ basics/basic3.js
@@ -6,3 +6,14 @@ function myFunction(str) {
 
    return str[str.length - 3] + str[str.length - 2] + str[str.length - 1];
 }
+const valores = [
+   ['abcdefg', 'efg'],
+   ['1234', '234'],
+   ['fgedcba', 'cba']
+];
+
+for (var i = 0; i < valores.length; i++) {
+   result = myFunction(valores[i][0]);
+   console.log("resultado: " + result + ", " + valores[i][1]);
+}
+
