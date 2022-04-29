6eace6f182d50c4d6c2cd87ad81574f94d65728f
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-04-28 21:02:13 -0600

    array

---
+++ basics/get-last-array-elements.js
@@ -0,0 +1,23 @@
+// Write a function that takes an array (a) and a number (n) as arguments
+// It should return the last n elements of a
+function myFunction (a, n) {
+
+  return a.slice(n - 1);
+   ;
+}
+
+console.log(myFunction([1, 2, 3, 4, 5], 2));
+console.log(myFunction([1, 2, 3], 6));
+console.log(myFunction([1, 2, 3, 4, 5, 6, 7, 8], 3));
+
+/*
+myFunction([1, 2, 3, 4, 5], 2)
+Expected
+[ 4, 5 ]
+myFunction([1, 2, 3], 6)
+Expected
+[ 1, 2, 3 ]
+myFunction([1, 2, 3, 4, 5, 6, 7, 8], 3)
+Expected
+[ 6, 7, 8 ]
+*/
