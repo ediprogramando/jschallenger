24ba9feabcea1347128a53221319bf5996de1146
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-04-29 21:02:13 -0600

    arraydos

--- arrays/javascript-arrays/get-last-array-elements.js
+++ arrays/javascript-arrays/get-last-array-elements.js
@@ -10,15 +10,3 @@ console.log(myFunction([1, 2, 3, 4, 5], 2));
 console.log(myFunction([1, 2, 3], 6));
 console.log(myFunction([1, 2, 3, 4, 5, 6, 7, 8], 3));
 
-/*
-myFunction([1, 2, 3, 4, 5], 2)
-Expected
-[ 4, 5 ]
-myFunction([1, 2, 3], 6)
-Expected
-[ 1, 2, 3 ]
-myFunction([1, 2, 3, 4, 5, 6, 7, 8], 3)
-Expected
-[ 6, 7, 8 ]
-
-*/
