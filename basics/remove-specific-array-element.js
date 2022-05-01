6c41d65a6daf47ce4c468f28ad32fced79500967
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-04-30 21:02:13 -0600

    remover

---
+++ basics/remove-specific-array-element.js
@@ -0,0 +1,12 @@
+// Write a function that takes an array (a) and a value (b) as argument
+// The function should clean a from all occurrences of b
+// Return the filtered array
+function
+myFunction
+(
+a, b
+)
+{
+
+return 
+}
