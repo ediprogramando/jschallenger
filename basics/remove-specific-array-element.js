6c3b3b0d5b9934ae13a64eb947a7e2a4ede5e77a
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-05-02 21:02:13 -0600

    arraytres

--- basics/remove-specific-array-element.js
+++ basics/remove-specific-array-element.js
@@ -12,3 +12,19 @@ a, b
 
 return 
 }
+
+
+/*
+myFunction([1,2,3], 2)
+Expected
+[1, 3]
+myFunction([1,2,'2'], '2')
+Expected
+[1, 2]
+myFunction([false,'2',1], false)
+Expected
+['2', 1]
+myFunction([1,2,'2',1], 1)
+Expected
+[2, '2']
+*/
