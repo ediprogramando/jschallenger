53f04f9186fdfa53c9d0fc3a9a84103f7a3a00ef
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-05-03 21:02:13 -0600

    arraycuatro

--- basics/remove-specific-array-element.js
+++ basics/remove-specific-array-element.js
@@ -3,14 +3,9 @@
 // Write a function that takes an array (a) and a value (b) as argument
 // The function should clean a from all occurrences of b
 // Return the filtered array
-function
-myFunction
-(
-a, b
-)
-{
+function myFunction(a, b) {
 
-return 
+  return 
 }
 
 
