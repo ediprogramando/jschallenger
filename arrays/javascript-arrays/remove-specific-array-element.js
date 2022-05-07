c706dc93265c04dfc416fe8dd07343fa9b407b51
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-05-06 21:02:13 -0600

    remove2

--- arrays/javascript-arrays/remove-specific-array-element.js
+++ arrays/javascript-arrays/remove-specific-array-element.js
@@ -13,10 +13,10 @@ var resultado = [];
   }
   return resultado;
 }
-console.log(myFunction([1,2,3], 2));
-console.log(myFunction([1,2,'2'], '2'));
-console.log(myFunction([false,'2',1], false));
-console.log(myFunction([1,2,'2',1], 1));
+console.log([1, 3], myFunction([1,2,3], 2));
+console.log([1, 2], myFunction([1,2,'2'], '2'));
+console.log(['2', 1], myFunction([false,'2',1], false));
+console.log([2, '2'], myFunction([1,2,'2',1], 1));
 
 /*
 myFunction([1,2,3], 2)
