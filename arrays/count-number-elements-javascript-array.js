c77c8bab6b71033cc5259d74d87201449d35f69b
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-05-09 21:02:13 -0600

    counttres

--- arrays/count-number-elements-javascript-array.js
+++ arrays/count-number-elements-javascript-array.js
@@ -1,13 +1,8 @@
 // Write a function that takes an array (a) as argument
 // Return the number of elements in a
-function
-myFunction
-(
-a
-)
-{
+function myFunction(a) {
 
-return 
+   return 
 }
 
 /*
