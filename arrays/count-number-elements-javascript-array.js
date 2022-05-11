c1a86a015863ce3b52f75b3baa2ffb8eef663f9d
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-05-10 21:02:13 -0600

    countcuatro

--- arrays/count-number-elements-javascript-array.js
+++ arrays/count-number-elements-javascript-array.js
@@ -5,6 +5,10 @@ function myFunction(a) {
    return 
 }
 
+console.log(4, myFunction([1,2,2,4]));
+console.log(3, myFunction([9,9,9]));
+console.log(5, myFunction([4,3,2,1,0]));
+
 /*
 myFunction([1,2,2,4])
 Expected
