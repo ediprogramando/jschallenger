463ad289665b96731c136cfc12bab80e1e549083
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-05-11 21:02:13 -0600

    countcinco

--- arrays/count-number-elements-javascript-array.js
+++ arrays/count-number-elements-javascript-array.js
@@ -1,3 +1,5 @@
+//https://www.jschallenger.com/javascript-arrays/count-number-elements-javascript-array/
+
 // Write a function that takes an array (a) as argument
 // Return the number of elements in a
 function myFunction(a) {
