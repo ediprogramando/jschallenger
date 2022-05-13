93804a3a052b7a7bf4265ecf6ea222a502b849d8
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-05-12 21:02:13 -0600

    conteofinal

--- arrays/count-number-elements-javascript-array.js
+++ arrays/count-number-elements-javascript-array.js
@@ -3,13 +3,13 @@
 // Write a function that takes an array (a) as argument
 // Return the number of elements in a
 function myFunction(a) {
-
-   return 
+   
+    return a.length;
 }
 
-console.log(4, myFunction([1,2,2,4]));
-console.log(3, myFunction([9,9,9]));
-console.log(5, myFunction([4,3,2,1,0]));
+console.log(myFunction([1,2,2,4]));
+console.log(myFunction([9,9,9]));
+console.log(myFunction([4,3,2,1,0]));
 
 /*
 myFunction([1,2,2,4])
