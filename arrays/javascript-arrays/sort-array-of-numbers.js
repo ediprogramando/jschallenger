efed739947ac8e4b09855504d0f9d46b3762c879
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-05-20 21:02:13 -0600

    sorttres

--- arrays/javascript-arrays/sort-array-of-numbers.js
+++ arrays/javascript-arrays/sort-array-of-numbers.js
@@ -1,3 +1,5 @@
+https://www.jschallenger.com/javascript-arrays/sort-array-of-numbers
+
 // Write a function that takes an array of numbers as argument
 // It should return an array with the numbers sorted in descending order
 function
