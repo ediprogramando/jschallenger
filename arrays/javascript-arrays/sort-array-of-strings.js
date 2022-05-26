3bb8e784607c9336b47ba6a7fdc518a7c1d55179
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-05-25 21:02:13 -0600

    arraydos

--- arrays/javascript-arrays/sort-array-of-strings.js
+++ arrays/javascript-arrays/sort-array-of-strings.js
@@ -10,3 +10,12 @@ arr
 
 return 
 }
+
+/*
+myFunction(['b', 'c', 'd', 'a'])
+Expected
+['a', 'b', 'c', 'd']
+myFunction(['z', 'c', 'd', 'a', 'y', 'a', 'w'])
+Expected
+['a', 'a', 'c', 'd', 'w', 'y', 'z']
+*/
