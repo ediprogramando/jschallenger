7bd458042d153d7d9f3a33cdd0845ccb2bdbc334
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-05-26 21:02:13 -0600

    arraytres

--- arrays/javascript-arrays/sort-array-of-strings.js
+++ arrays/javascript-arrays/sort-array-of-strings.js
@@ -1,3 +1,5 @@
+https://www.jschallenger.com/javascript-arrays/sort-array-of-strings
+
 // Write a function that takes an array of strings as argument
 // Sort the array elements alphabetically
 // Return the result
