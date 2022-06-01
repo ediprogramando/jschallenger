31b44aa63ed4a82ccaff51b442da0eea172241d6
Alfredo Gonzalez <alfredogonzalez271993@gmail.com>
2022-05-31 21:02:13 -0600

    promediodos

--- arrays/javascript-arrays/return-average-of-array.js
+++ arrays/javascript-arrays/return-average-of-array.js
@@ -1,6 +1,24 @@
+https://www.jschallenger.com/javascript-arrays/return-average-of-array
+
 // Write a function that takes an array of numbers as argument
 // It should return the average of the numbers
 function myFunction(arr) {
 
    return 
 }
+
+console.log(myFunction([10,100,40]));
+console.log(myFunction([10,100,1000]));
+console.log(myFunction(-50,0,50,200));
+
+/*
+myFunction([10,100,40])
+Expected
+50
+myFunction([10,100,1000])
+Expected
+370
+myFunction([-50,0,50,200])
+Expected
+50
+*/
